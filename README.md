cab470c80f834fac1d2b315dccc5399dee9cfe4c
rahul97701 <111692381+rahul97701@users.noreply.github.com>
2024-10-18 17:29:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,54 @@
+# React Developer Portfolio template
+#### Live Demo https://react-portfolio-v1.netlify.app/
+#### Figma Design https://www.figma.com/community/file/1116246660507537002
+### Preview ( Dark Mode )
+
+
+## Follow Below instructions to add your deatils in the portfolio.
+
+### Change and customize every section according to your need, All you need to do is edit `/src/Details.js`
+
+#### Open `/src/Details.js` & modify it as per your need.
+
+```javascript
+/ Enter your Personal Details here
+export const personalDetails = {
+  name: "Rahuul Khiichi",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I have 2 years of experience as a Front-end web developer in developing and designing user experiences of  
+Internet/Intranet applications using HTML, HTML5, XML, CSS3, JavaScript, React, React Native Redux, 
+ES6, Saga, Thunk Web pack, jQuery, JSON, Node.js, JQUERY Bootstrap.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/rahuul-khiichi-62937a88/",
+  github: "https://github.com/rahul97701/disney-plus-clone-app",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `RadixCube Soultions Pvt Ltd`,
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "May 2022 - Present",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = { ... };
+
+// Tech Stack and Tools
+export const techStackDetails = { ... };
+
+// Enter your Project Details here
+export const projectDetails = [ ... ];
+
+// Enter your Contact Details here
+export const contactDetails = { ... };
+```
---
+++ src/Details.js
@@ -0,0 +1,183 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo3.svg";
+import logo from "./assets/logo3.svg";
+// Profile Image
+import profile from "./assets/profile.png";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/Disney-project.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Rahuul Khiichi",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I have 2 years of experience as a Front-end web developer in developing and designing user experiences of  
+Internet/Intranet applications using HTML, HTML5, XML, CSS3, JavaScript, React, React Native Redux, 
+ES6, Saga, Thunk Web pack, jQuery, JSON, Node.js, JQUERY Bootstrap.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/rahuul-khiichi-62937a88/",
+  github: "https://github.com/rahul97701/disney-plus-clone-app",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `RadixCube Soultions Pvt Ltd`,
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "May 2022 - Present",
+  },
+  // {
+  //   Position: "Internship",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Internship",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+  // {
+  //   Position: "Internship",
+  //   Company: `Company Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Internship",
+  //   Duration: "Sep 2021 - Dec 2021",
+  // },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Full Stack Developer",
+    Company: "Edureka",
+    Location: "Online",
+    Type: "Full Time",
+    // Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Automation Testing",
+    Company: "SimpliLearn",
+    Location: "Online",
+    Type: "Full Time",
+    // Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Mechanical Engineering",
+    Company: `RKDF College Of Engineering`,
+    Location: "Bhopal",
+    Type: "Full Time",
+    Duration: "June 2009 - June 2013"
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Disney-plus-app",
+    image: projectImage1,
+    description: `Disney apps have been designed to provide a seamless experience for users across multiple devices: `,
+    techstack: "HTML/CSS, JavaScript, React",
+    previewLink: "https://disney-plus-app.vercel.app/",
+    githubLink: "https://github.com/rahul97701/disney-plus-clone-app",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "rahuulkhiichi05@gmail.com",
+  phone: "+91 9238044903",
+};
